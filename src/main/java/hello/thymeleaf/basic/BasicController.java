930790654aa4d6ccdb20e689ab2f16ecc31c4858
DevKs-Yet <ksang0324@gmail.com>
2022-03-14 13:11:28 +0900

    20220314 - 유틸리티 객체와 날짜

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -79,6 +80,13 @@ public class BasicController {
             log.info("hello - data = {}", data);
             return "Hello " + data;
         }
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        log.info("date - model = {}", model);
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
     }
 
 }
